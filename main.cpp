4bc77fa4ab60e37ba5af1ee47dc9b9b62d845fa8
rsl_cs <lohanravindra@gmail.com>
2021-02-22 15:14:34 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,11 @@
 using namespace std;
 
 int main(){
-  Mat img1 = imread(empty.jpg);
+  Mat img1 = imread("empty.jpg");
+  if (img1.empty()){
+    cout<<"Error loading the image"<<endl;
+    return -1;
+  }
+  namedWindow("My Window",1);
+  imshow("My Window",img1);
 }
